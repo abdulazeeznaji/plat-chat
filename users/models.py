2596172465d63a8fe8d3d387e3732f35c3e281d9
abdulazeeznaji <abdulazeeznaji@gmail.com>
2020-07-22 23:13:08 +0300

    Fix typo

--- users/models.py
+++ users/models.py
@@ -5,10 +5,11 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
     PermissionsMixin
 
+
 class MyUserManager(BaseUserManager):
     def _create_user(self, email, password, user_name, **extra_fields):
         email = self.normalize_email(email)
-        user = self.model(email=email, user_name=user_name **extra_fields)
+        user = self.model(email=email, user_name=user_name, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
