932e36a6fcdc56663fee2129c9c167ab3519713c
abdulazeeznaji <abdulazeeznaji@gmail.com>
2020-07-25 21:03:47 +0300

    Handle both create post & post detail function handle CRUD

--- posts/views.py
+++ posts/views.py
@@ -3,10 +3,33 @@ from rest_framework.response import Response
 from posts.models import Posts
 from .serializers import PostsSerializer
 from django.http.response import JsonResponse
+from rest_framework.parsers import JSONParser
+from rest_framework import status
 
 
 @api_view(['GET', 'POST', 'DELETE'])
 def get_posts(request):
-    posts = Posts.objects.all()
-    posts_serializer = PostsSerializer(posts, many=True)
-    return JsonResponse(posts_serializer.data, safe=False)
+    if request.method == 'GET':
+        posts = Posts.objects.all()
+        posts_serializer = PostsSerializer(posts, many=True)
+        return JsonResponse(posts_serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        posts_data = JSONParser().parse(request)
+        post_serializer = PostsSerializer(data=posts_data)
+        if post_serializer.is_valid():
+            post_serializer.save()
+            return JsonResponse(post_serializer.data, status=status.HTTP_201_CREATED)
+        return JsonResponse(post_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def post_detail(request, pk):
+    try:
+        post = Posts.objects.get(pk=pk)
+    except Posts.DoesNotExist:
+        return JsonResponse({'message': 'The tutorial does not exist'}, status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'DELETE':
+        post.delete()
+        return JsonResponse({'message': 'Tutorial was deleted successfully!'}, status=status.HTTP_204_NO_CONTENT)
