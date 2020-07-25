bd14b7ce64257827e7b71772dd51e44a0c759bce
abdulazeeznaji <abdulazeeznaji@gmail.com>
2020-07-25 20:50:53 +0300

    Add delete icon to post card and add some styling

--- static/src/components/SinglePost.js
+++ static/src/components/SinglePost.js
@@ -3,9 +3,22 @@ import styled from 'styled-components';
 import Grid from '@material-ui/core/Grid';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
+import DeleteIcon from '@material-ui/icons/Delete'
+import red from '@material-ui/core/colors/red';
+
+
 
 const Panel = styled.div`
 	margin: 5px;
+`;
+
+
+const Delete = styled(DeleteIcon)`
+	position: relative;
+	cursor: pointer;
+	right: 15px;
+	top: 9px;
+	float:right;
 `;
 
 class SinglePost extends Component {
@@ -15,12 +28,15 @@ class SinglePost extends Component {
 		this.state = {open: false}
 	}
 	render() {
-		const { post } = this.props;
+		const { post, deletePost } = this.props;
 		return(
 			<Panel>
 				<Grid container style={{margin: 10}}>
 					<Grid item xs={10} sm={10} md={10}>
 						<Typography style={{fontSize: 18}}>{post.body}</Typography>
+					</Grid>
+					<Grid item xs={2} sm={2} md={2}>
+						<Delete nativeColor={red[600]} onClick={() => deletePost(post.id)}/>
 					</Grid>
 				</Grid>
 				<Fragment>
