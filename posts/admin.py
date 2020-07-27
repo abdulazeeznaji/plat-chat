3c6d532461699b858ffca4b3e627c76cd46baae2
abdulazeeznaji <abdulazeeznaji@gmail.com>
2020-07-27 03:35:01 +0300

    Register posts and users django admin

--- posts/admin.py
+++ posts/admin.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from .models import Posts
 
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+admin.site.register(Posts)
--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from .models import User
 
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+admin.site.register(User)
